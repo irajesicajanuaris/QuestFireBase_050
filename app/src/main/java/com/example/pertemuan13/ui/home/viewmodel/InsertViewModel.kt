872eda61b9a6078b2c7c4db0da9c4bdcfdb1567f
irajesicajanuaris <jjiraaa1@gmail.com>
2025-01-10 20:05:25 +0700

    menambahkan data class FormErrorState  dan data class InsertUiState pada InsertViewModel.kt

--- app/src/main/java/com/example/pertemuan13/ui/home/viewmodel/InsertViewModel.kt
+++ app/src/main/java/com/example/pertemuan13/ui/home/viewmodel/InsertViewModel.kt
@@ -4,6 +4,25 @@ import com.example.pertemuan13.model.Mahasiswa
 
 
 
+data class InsertUiState(
+    val insertUiEvent: MahasiswaEvent = MahasiswaEvent(),
+    val isEntryValid: FormErrorState = FormErrorState()
+)
+
+data class FormErrorState(
+    val nim: String? = null,
+    val nama: String? = null,
+    val jenisKelamin: String? = null,
+    val alamat: String? = null,
+    val kelas: String? = null,
+    val angkatan: String? = null,
+){
+    fun isValid() : Boolean {
+        return nim == null && nama == null && jenisKelamin == null &&
+                alamat == null && kelas == null && angkatan == null
+    }
+}
+
 //data class variabel yang menyimpan data input form
 data class MahasiswaEvent(
     val nim: String = "",
